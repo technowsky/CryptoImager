004b2df6e870f08ec3dd00640fc90ab52f7958f7
Mikołaj Markowski <mikolaj.a.markowski@gmail.com>
2025-05-30 23:49:46 +0200

    settingManager refactored as a Python module, waiting to be implemented to other files

--- classes/settingManager.py
+++ classes/settingManager.py
@@ -2,44 +2,50 @@ import json
 import os
 from pathlib import Path
 
-class settingManager:
-    _instance = None
-    _settings_path = "setting.json"
+
+SETTINGS_PATH = Path("settings.json")
+DEFAULT_JSON_SETTINGS = {                                           #defult data settings
+            "save_dir": str(Path("./").resolve()) + os.sep,
+            "coding_method": 0,  # 0 - diagonal, 1 - From first to last, 2 - "Random", 3 - "Spiral from center"
+            "save_custom_name": 1, # 0 - global, 1 - Ask every time, 2 - Same as selected image
+            "save_prefix": ""
+        }
+
+current_json_settings = DEFAULT_JSON_SETTINGS.copy()
+
+def save_settings():
+    global current_json_settings
+    try:
+        with open(SETTINGS_PATH, "w", encoding="utf-8") as f:
+                json.dump(current_json_settings, f)
+    except (IOError, PermissionError):
+            print("Saving/Loading settings.json file failed due to permission error. Check creating file permissions.")
 
 
+def load_settings():
+    global current_json_settings
 
-    def __init__(self):
-        raise 'Singleton class. Call function ".get_instance()" method'
-    
-    def get_instance(cls):
-        if cls._instance == None:
-            cls._instance = cls.__new__(cls)
-            cls._instance._load_settings()
-        return cls._instance
+    try:
+        with open(SETTINGS_PATH, "r", encoding="utf-8") as f:
+            current_json_settings = json.load(f)
+    except FileNotFoundError:
+        save_settings()
+    except (IOError, json.JSONDecodeError) as e:
+        raise json.JSONDecodeError("Failed to decode json data from file")
 
- 
-    def _load_settings(self):
-        try:
-            with open(self._settings_path, "r") as f:
-                self._json_settings = json.load(f)
-                print(self._json_settings)
-        except:
-            self._json_settings = {
-                "save_dir": str(Path("./").resolve()) + os.sep,
-                "coding_method": 0,  # 0 - diagonal, 1 - From first to last, 2 - "Random", 3 - "Spiral from center"
-                "save_custom_name": 1, # 0 - global, 1 - Ask every time, 2 - Same as selected image
-                "save_prefix": ""
-            }
-            with open(self._settings_path, "w") as f:
-                json.dump(self._json_settings, f)
-        
-    def __str__(self):
-        print(self._json_settings)
+def validate_settings():
+    ...
+    #for future validation of data in settings.json. Comperes setting.json data to  DEFAULT_JSON_SETTINGS
+
+def get(key:str):
+    if key not in current_json_settings:
+        raise KeyError(f"{key} not found")
+    return current_json_settings[key]
+
+def set(key:str, value):
+    if key not in current_json_settings:
+        raise KeyError(f"{key} not found")
+    current_json_settings[key] = value
 
 
-
-        
-
-
-test = settingManager().get_instance()
-print(test)
+load_settings()     
